ba19a8dcb89cd4b7a185080631b66c7b589355c2
amad3v <55244128+amad3v@users.noreply.github.com>
2022-01-15 14:39:54 +0000

    Header file to include all types

---
+++ src/artypes.h
@@ -0,0 +1,20 @@
+/**
+ * @file artypes.h
+ * @date 04.01.22
+ * @author amad3v (amad3v@gmail.com)
+ * @version 0.0.1
+ *
+ * @brief 3D vector type.
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __LIB_CUSTOM_TYPE_CUSTOM_H__
+#define __LIB_CUSTOM_TYPE_CUSTOM_H__
+
+#include "vector.h"
+#include "quaternion.h"
+#include "matrix.h"
+
+#endif /* __LIB_CUSTOM_TYPE_CUSTOMR_H__ */
