b4862c84f3fef803ff14bd663aad4931fa48c3a2
amad3v <55244128+amad3v@users.noreply.github.com>
2022-01-15 14:54:05 +0000

    change: rename `RAD_TO_DEG` to `RAD2DEG`

--- src/def.h
+++ src/def.h
@@ -14,7 +14,7 @@
 #define __LIB_CUSTOM_TYPES_DEF_H__
 
 namespace cst {
-const float RAD_TO_DEG { 57.295779513082320876798154814105 };
+const float RAD2DEG { 57.295779513082320876798154814105 };
 
 /**
  * @brief Computes the square of a number.
